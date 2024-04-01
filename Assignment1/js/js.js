5af11a265d4338b8cc37017bf673ed9698bbb93c
Dani <119665698+S4nxez@users.noreply.github.com>
2024-04-01 21:53:48 +0200

    final corrections on js and improved .gitignore

--- Assignment1/js/js.js
+++ Assignment1/js/js.js
@@ -3,6 +3,14 @@ document.getElementById("email").addEventListener("change", validate_email);
 document.getElementById("password").addEventListener("change", validate_pwd);
 document.getElementById("confirmpassword").addEventListener("change", validate_confirm_pwd);
 document.getElementById("empty").addEventListener("click", empty);
+document.getElementById("miFormulario").addEventListener("submit", function(event) {
+	const pwd = document.getElementById("password").value;
+	const pwdConfirm = document.getElementById("confirmpassword").value;
+
+	if (pwd !== pwdConfirm) {
+		event.preventDefault();
+	}
+	});
 
 function validate_name() {
 	const numb = document.getElementById("name");
@@ -35,7 +43,17 @@ function validate_pwd() {
 	const pPwd = document.getElementById("valMessageP");
 
 	if(!pwd.checkValidity()){
-		pPwd.innerHTML = pwd.validationMessage;
+		if (pwd.value.length < 8) {
+			pPwd.innerHTML = "The password must have more than 8 characters.";
+		} else if (!/[a-z]/.test(pwd.value)) {
+			pPwd.innerHTML = "The password must contain at least one lowercase letter.";
+		} else if (!/[A-Z]/.test(pwd.value)) {
+			pPwd.innerHTML = "The password must contain at least one uppercase letter.";
+		} else if (!/\d/.test(pwd.value)) {
+			pPwd.innerHTML = "The password must contain at least one number.";
+		} else {
+			pPwd.innerHTML = pwd.validationMessage;
+		}
 		pPwd.style.backgroundColor = "red";
 	}else {
 		pPwd.innerHTML = null;
@@ -48,7 +66,7 @@ function validate_confirm_pwd(){
 	const pwdConfirm = document.getElementById("confirmpassword").value;
 	const pPwd = document.getElementById("valMessageC");
 
-	if (pwd !== pwdConfirm){
+	if (!(pwd == pwdConfirm)){
 		pPwd.innerHTML = "Passwords should match";
 		pPwd.style.backgroundColor = "red";
 	}else {
