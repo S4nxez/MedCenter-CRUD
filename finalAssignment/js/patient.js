af2bcdb96dd2ad1595c777a6dbe16f7f688d0854
Dani <119665698+S4nxez@users.noreply.github.com>
2024-04-25 20:17:42 +0200

    minor improvements

--- finalAssignment/js/patient.js
+++ finalAssignment/js/patient.js
@@ -28,15 +28,110 @@ function createPatientRow(patient) {
 		"<td>" + patient.phone +"</td>" +
 		"<td><button class='btn btn-info btn-sm' >Get info</button></td>" +
 		"<td><button class='btn btn-danger btn-sm' >Delete</button></td>" +
-		"<td><button class='btn btn-primary btn-sm' >Update</button></td>";
+		"<td><button class='btn btn-primary btn-sm ms-2' >Update</button></td>";
 
 	//row.querySelector(".btn-info").addEventListener("click", getInfo);
 	//row.querySelector(".btn-danger").addEventListener("click", deletePatient);
-	//row.querySelector(".btn-primary").addEventListener("click", showUpdatePatientModalW);
+	row.querySelector(".btn-primary").addEventListener("click", showUpdatePatientModal);// esto habria que cambiarlo pero no se si lo arregló al final
 	return row;
 }
 
 function addPatientModal() {
 	let addPatientModal = new bootstrap.Modal(document.getElementById('addPatientModal'));
 	addPatientModal.show();
+}
+
+function addPatient() {
+	let patientName=document.getElementById("addPatientName").value;
+	let patientPhone=document.getElementById("addPatientPhone").value;
+
+	let patient= {
+		id:0,
+		name: patientName,
+		phone: patientPhone
+	};
+
+	fetch ("https://informatica.iesquevedo.es/marcas/patients", {
+		method: 'POST',
+		headers: {
+			'Content-Type' : 'application/json'
+		},
+		body: JSON.stringify(patient)
+	}).then (response => {
+		if(!response.ok) {
+			throw new Error('Network response is not ok');
+		}
+		return response.json()
+		}).catch (error => {
+			console.error("Error in the fetch:", error)
+		})
+}
+
+function showUpdatePatientModal(event) {
+	let row = event.target.parentNode.parentNode;
+	let cells = row.getElementByTagName("td");
+	let id = cells[0].innerText;
+	let name = cells[1].innerText;
+	document.getElementById("updatepatientId").value=patientId;
+	document.getElementById("updatepatentName").value=patientName;
+	document.getElementById("updatePatientB").addEventListener("click", );
+	let updatePatientModal = new bootstrap.Modal(document.getElementById)
+
+}
+
+function updatePatient() {
+	let patientId = document.getElementById("updatepatientId").value;
+	let patientName = document.getElementById("updatepatientName").value;
+
+	let patient = {
+		id: patientId,
+		name: patientName,
+		phone: patientName
+	}
+	fetch ("https://informatica.iesquevedo.es/marcas/patients", {
+		method: 'PUT',
+		headers: {
+			'Content-Type' : 'application/json'
+		},
+		body: JSON.stringify(patient)
+	})
+	.then (response => {
+		if(!response.ok) {
+			throw new Error('Network response is not ok');
+		}
+		return response.json()
+	}).then (data => {
+		console.log('Patient updated succesfully' + data)
+		$('#updatePatientModal').modal('hide');
+		//modify data
+		row.cells[0].innerText=patient.name;
+		row.cells[2].innerText=patient.phone;
+	})
+	.catch (error => {
+		console.error("Error in the fetch:", error)
+	})
+}
+function deletePatient(event) {
+	let row=event.target.parentNode.parentNode;
+	let patientId=row.cells[0].innerText;
+
+	fetch ("https://informatica.iesquevedo.es/marcas/patients/${patientId}?confirm=true", {
+		method: 'DELETE',
+		heraders: {
+			'Content-Type' : 'application/json'
+		}
+	})
+	.then (response => {
+		if (!response.ok) {
+			throw new Error ('Network response is not ok');
+		}
+		return response.json()
+	})
+	.then(data => {
+		console.log("patient deleted succesfully: ", data);
+		document.getElementById("patientTableBody").removeChild(row) //revisar
+	})
+	.catch (error => {
+		console.error('Error updating patient: ', error);
+	});
 }
